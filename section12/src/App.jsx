217f588fcc4b8a62e004993316d51a6d44f27e1e
Sumsan38 <ndanl4647@gmail.com>
2024-09-22 15:41:48 +0900

    12.8) 일기 관리 기능 구현하기 1

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,30 +1,44 @@
 import "./App.css"
-import {Route, Routes, useNavigate} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Diary from "./pages/Diary.jsx";
 import New from "./pages/New.jsx"
+import Edit from "./pages/Edit.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Button from "./components/Button.jsx";
 import Header from "./components/Header.jsx";
+import {useReducer} from "react";
 
+const mockData = [
+    {
+        id: 1,
+        createDate: new Date().getTime(),
+        emotionId: 1,
+        content: "1번 일기 내용"
+    },
+    {
+        id: 2,
+        createDate: new Date().getTime(),
+        emotionId: 2,
+        content: "2번 일기 내용"
+    }
+]
+
+function reducer(state, action) {
+    return state;
+}
 
 function App() {
+    const [data, dispatch] = useReducer(reducer, [mockData]);
+
     return (
         <>
             <Header title={"Header"} leftChild={<Button text={"left"}/>} rightChild={<Button text={"right"}/>}/>
-            <Button text={"기본"} onClick={() => {
-                console.log("clicked");
-            }}/>
-            <Button text={"긍정"} type={"POSITIVE"} onClick={() => {
-                console.log("clicked");
-            }}/>
-            <Button text={"부정"} type={"NEGATIVE"} onClick={() => {
-                console.log("clicked");
-            }}/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/new" element={<New/>}/>
                 <Route path="/diary/:id" element={<Diary/>}/>
+                <Route path="/edit/:id" element={<Edit/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
         </>
